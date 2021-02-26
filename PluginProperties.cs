e121897e56bfe1f2e87ac1af0985e80244dd23d3
Sakura Isayeki <sakura.isayeki@nodsoft.net>
2021-02-26 09:24:18 +0100

    Add Logging steps for Loading/Unloading

--- PluginProperties.cs
+++ PluginProperties.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Nodsoft.YumeChan.PluginBase;
 using System;
 using System.Threading.Tasks;
@@ -6,8 +8,28 @@ namespace Nodsoft.YumeChan.Essentials
 {
 	public class PluginProperties : Plugin
 	{
+		private readonly ILogger<PluginProperties> logger;	
+
+		public PluginProperties(ILogger<PluginProperties> logger)
+		{
+			this.logger = logger;
+		}
+
 		public override string PluginDisplayName { get; } = "Yume-Chan Essentials";
 
 		public override bool PluginStealth { get; } = false;
+
+
+		public override async Task LoadPlugin()
+		{
+			await base.LoadPlugin();
+			logger.LogInformation("Ready.");
+		}
+
+		public override async Task UnloadPlugin()
+		{
+			logger.LogInformation("Unloading...");
+			await base.UnloadPlugin();
+		}
 	}
 }
