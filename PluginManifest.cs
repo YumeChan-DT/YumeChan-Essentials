b14de7181ebae412661c076fd8ee2c40dc8f609f
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-04-24 13:09:54 +0200

    Update to PluginBase 3.0

--- PluginManifest.cs
+++ PluginManifest.cs
@@ -4,32 +4,31 @@ using YumeChan.PluginBase;
 using System;
 using System.Threading.Tasks;
 
-namespace YumeChan.Essentials
+namespace YumeChan.Essentials;
+
+public class PluginManifest : Plugin
 {
-	public class PluginManifest : Plugin
+	private readonly ILogger<PluginManifest> logger;
+
+	public PluginManifest(ILogger<PluginManifest> logger)
 	{
-		private readonly ILogger<PluginManifest> logger;
+		this.logger = logger;
+	}
 
-		public PluginManifest(ILogger<PluginManifest> logger)
-		{
-			this.logger = logger;
-		}
+	public override string DisplayName => "Yume-Chan Essentials";
 
-		public override string DisplayName { get; } = "Yume-Chan Essentials";
-
-		public override bool StealthMode { get; } = false;
+	public override bool StealthMode => false;
 
 
-		public override async Task LoadAsync()
-		{
-			await base.LoadAsync();
-			logger.LogInformation("Ready.");
-		}
+	public override async Task LoadAsync()
+	{
+		await base.LoadAsync();
+		logger.LogInformation("Ready.");
+	}
 
-		public override async Task UnloadAsync()
-		{
-			logger.LogInformation("Unloading...");
-			await base.UnloadAsync();
-		}
+	public override async Task UnloadAsync()
+	{
+		logger.LogInformation("Unloading...");
+		await base.UnloadAsync();
 	}
 }
